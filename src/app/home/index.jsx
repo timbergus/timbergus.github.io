8d9e758bb8bd087be25a7750eb13118d48d175aa
Gustavo Muñoz <timbergus@gmail.com>
2016-04-25 14:55:49 +0200

    Lint the app.

--- src/app/home/index.jsx
+++ src/app/home/index.jsx
@@ -1,51 +1,45 @@
-import React from 'react';
+import React from 'react'
 
-class Home extends React.Component {
+export default class Home extends React.Component {
 
-    constructor (props, context) {
-
-        super(props, context);
-
-        this.state = {
-            txt: 'state message...'
-        };
-    }
-
-    update () {
-
-        setTimeout(() => {
-            this.setState({
-                txt: 'new state message :)'
-            });
-        }, 5000);
-    }
-
-    componentDidMount () {
-
-        this.update();
-    }
-
-    render () {
-
-        return (
-            <div>
-                <h1>Timbergus Site Under Construction!</h1>
-                <p>This is a {this.props.txt}</p>
-                <p>This is a {this.state.txt}</p>
-                <p>Counter: { this.props.counter }</p>
-                <button onClick={ this.props.incrementCounter }>+</button>
-                <button onClick={ this.props.decrementCounter }>-</button>
-            </div>
-        );
-    }
-}
-
-Home.propTypes = {
+  static propTypes = {
     txt: React.PropTypes.string.isRequired
-};
+  }
 
-Home.defaultProps = {
+  static defaultProps = {
     txt: 'props message...'
-};
+  }
 
-export default Home;
+  constructor (props, context) {
+    super(props, context)
+
+    this.state = {
+      txt: 'state message...'
+    }
+  }
+
+  update () {
+    setTimeout(() => {
+      this.setState({
+        txt: 'new state message :)'
+      })
+    }, 5000)
+  }
+
+  componentDidMount () {
+    this.update()
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Timbergus Site Under Construction!</h1>
+        <p>This is a {this.props.txt}</p>
+        <p>This is a {this.state.txt}</p>
+        <p>Counter: { this.props.counter }</p>
+        <button onClick={ this.props.incrementCounter }>+</button>
+        <button onClick={ this.props.decrementCounter }>-</button>
+      </div>
+    )
+  }
+}
