0af7333af1b9f70b2a22c102e66e68011795f024
Gustavo Muñoz <timbergus@gmail.com>
2016-04-25 15:35:01 +0200

    Fix the router.

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,41 +1,40 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
-import { incrementCounter, decrementCounter } from './store/counter';
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Router, Route, hashHistory } from 'react-router'
+import { incrementCounter, decrementCounter } from './store/counter'
 
-import Home from './home/index.jsx';
-
-class HomeWrapper extends React.Component {
-    
-    render () {
-        return (<Home txt="This is working just fine :)" />);
-    }
-};
+import Home from './home/index.jsx'
 
 const mapStateToProps = state => {
-    return {
-        counter: state.counter
-    };
-};
+  return {
+    counter: state.counter
+  }
+}
 
 const mapDispatchToProps = dispatch => {
-    return {
-        incrementCounter: bindActionCreators(incrementCounter, dispatch),
-        decrementCounter: bindActionCreators(decrementCounter, dispatch)
-    }
-};
+  return {
+    incrementCounter: bindActionCreators(incrementCounter, dispatch),
+    decrementCounter: bindActionCreators(decrementCounter, dispatch)
+  }
+}
 
-const NewHome = connect(mapStateToProps, mapDispatchToProps)(HomeWrapper);
+const NewHome = connect(mapStateToProps, mapDispatchToProps)(Home)
+
+class HomeWrapper extends React.Component {
+
+  render () {
+    return (<NewHome txt='This is working just fine :)' />)
+  }
+};
 
 export default class Routes extends React.Component {
 
-    render () {
-
-        return (
-            <Router history={ hashHistory }>
-                <Route path="/" component={ NewHome } />
-            </Router>
-        );
-    }
+  render () {
+    return (
+      <Router history={ hashHistory }>
+        <Route path='/' component={ HomeWrapper } />
+      </Router>
+    )
+  }
 }
