3eabf7d6fec6916ad178c5d0bf0ebc887fc6898d
Gustavo Muñoz <timbergus@gmail.com>
2016-04-25 17:03:37 +0200

    Build.

--- src/app/home/index.jsx
+++ src/app/home/index.jsx
@@ -17,6 +17,8 @@ export default class Home extends React.Component {
     this.state = {
       txt: 'state message...'
     }
+
+    this.names = ['pepe', 'luis', 'juan']
   }
 
   update () {
@@ -38,6 +40,7 @@ export default class Home extends React.Component {
         <p>{ this.props.txt }</p>
         <p className={ classNames({ visible: this.props.counter === 5 }) }>This is a { this.state.txt }</p>
         <p>Counter: { this.props.counter }</p>
+        <ul className='list'>{ this.names.map((name, index) => <li key={ `name${index}` }>This is { name }</li>) }</ul>
         <button onClick={ this.props.incrementCounter }>+</button>
         <button onClick={ this.props.decrementCounter }>-</button>
       </div>
