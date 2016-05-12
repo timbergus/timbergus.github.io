d536aad10e55fda9acffc7a6604f023fdc7c02b2
Gustavo Muñoz <timbergus@gmail.com>
2016-05-12 22:03:26 +0200

    Bugs fixed.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-    entry: path.join(__dirname, 'src', 'app', 'index.jsx'),
-    output: {
-        path: path.join(__dirname, 'web'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        inline: true,
-        progress: true,
-        color: true,
-        contentBase: './web',
-        port: 3333
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.styl$/,
-                exclude: /node_modules/,
-                loader: 'style!css!stylus'
-            },
-            {
-                test: /\.js?$|\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel'
-            }
-        ]
-    }
-};
+  entry: path.join(__dirname, "src", "app", "index.jsx"),
+  output: {
+    path: path.join(__dirname, "web"),
+    publicPath: "/web",
+    filename: "bundle.js"
+  },
+  devServer: {
+    inline: true,
+    progress: true,
+    color: true,
+    contentBase: __dirname,
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.styl$/,
+        exclude: /node_modules/,
+        loader: "style!css!stylus"
+      },
+      {
+        test: /\.js?$|\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel"
+      }
+    ]
+  }
+}
