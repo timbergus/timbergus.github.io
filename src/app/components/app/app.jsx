c0b913eb7174116cc6d3160ab5005c0305f6d9df
Gustavo Muñoz <timbergus@gmail.com>
2018-01-28 19:18:30 +0100

    Finished for now.

--- src/app/components/app/app.jsx
+++ src/app/components/app/app.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import { src, trace } from 'images/meme.png';
-// import meme from 'images/meme.png';
 
 import config from './app.json';
 
@@ -27,7 +26,6 @@ export class App extends Component {
         <div className="image-container">
           <img src={trace} alt="meme" />
           <img src={src} alt="meme" className="hidden" onLoad={this.handleLoad} />
-          {/* <img src={meme} alt="meme" /> */}
         </div>
 
       </main>
