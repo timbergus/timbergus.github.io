5f85a7bd4f8ce4fd6a3d2314d4895b8fdcb83b3d
Gustavo Muñoz <timbergus@gmail.com>
2020-05-29 20:15:06 +0200

    Updates

--- src/app/utils/math.js
+++
@@ -1,7 +0,0 @@
-import mathWasm from './math.wasm';
-
-export const getRandom = (limit = 10) => Math.ceil(Math.random() * limit);
-
-export const add = (a = 0, b = 0) => mathWasm().then(response => {
-  return response.instance.exports.add(a, b);
-});
